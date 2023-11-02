f9965b429aac90def1056025b4c83a30af782753
Petra Zemanová <zemape42@gmail.com>
2023-11-02 22:42:55 +0100

    Functionality completed

---
+++ src/components/CreditCard/index.jsx
@@ -0,0 +1,35 @@
+import { useRef } from 'react';
+import './style.css';
+import { useEffect } from 'react';
+
+const CreditCard =() => {
+    const inputRef = useRef();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
+
+
+    const focusNextInput = (input) => {
+       input.nextSibling.focus();
+    }
+
+    const handleChange = (event) => {
+        (event.target.value).length === 4 ? focusNextInput(event.target) : null;
+    }
+
+
+    return(
+        <div className="credit-card">
+            <h2>Zadeje číslo kreditní karty:</h2>
+            <form>
+                <input type="text" id='input1' onChange={(e) => handleChange(e)} ref={inputRef}/>
+                <input type="text" id='input2' onChange={(e) => handleChange(e)}/>
+                <input type="text" id='input3' onChange={(e) => handleChange(e)}/>
+                <input type="text" id='input4' onChange={(e) => handleChange(e)}/>
+            </form>
+        </div>
+    )
+}
+
+export default CreditCard;
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,10 @@
+import CreditCard from '../../components/CreditCard';
+import './style.css';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <CreditCard />
+    </div>
+  );
+};
